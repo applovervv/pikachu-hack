6d0fb59dd875ea4d7a52715b6398d58ae78c23af
applovervv <77960740+applovervv@users.noreply.github.com>
2023-05-15 10:20:41 +0900

    first commit

---
+++ Pikachuhack/hacktest/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace hacktest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("굴림", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.checkBox2.Location = new System.Drawing.Point(21, 65);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(221, 36);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Ignore crash";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Font = new System.Drawing.Font("굴림", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.checkBox4.Location = new System.Drawing.Point(21, 12);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(152, 36);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "Autowin";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Player 1",
+            "Player 2 (Default)"});
+            this.comboBox1.Location = new System.Drawing.Point(179, 20);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(152, 20);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "Player 2 (Default)";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 250);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox2);
+            this.Name = "Form1";
+            this.Text = "피카츄배구핵";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Pikachuhack/hacktest/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Memory;
+
+namespace hacktest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+             Mem memory = new Mem();
+            int PID = memory.GetProcIdFromName("pika.exe"); //프로세스 아이디
+
+            if (PID > 0)
+            {
+                memory.OpenProcess(PID); 
+
+                if (checkBox2.Checked) 
+                {
+                    memory.WriteMemory("pika.exe+3C27", "bytes", "0xEB 0x76");
+                }
+                else
+                {
+                    memory.WriteMemory("pika.exe+3C27", "bytes", "0x75 0x76");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Run the pika.exe");
+            }
+        }
+
+   
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            Mem memory = new Mem();
+            int PID = memory.GetProcIdFromName("pika.exe"); //프로세스 아이디
+
+            if (PID > 0)
+            {
+                memory.OpenProcess(PID);
+
+                if (checkBox4.Checked)
+                {
+                    comboBox1.Enabled = true;
+                    memory.WriteMemory("pika.exe+3C58", "bytes", "0x90 0x90 0x90");
+                    memory.WriteMemory("pika.exe+3C5B", "bytes", "0xEB 0x10");  
+                }
+                else
+                {
+                    comboBox1.Enabled = false;
+                    memory.WriteMemory("pika.exe+3C58", "bytes", "0x39 0x46 0x40");
+                    memory.WriteMemory("pika.exe+3C5B", "bytes", "0x7D 0x10");
+
+                    memory.WriteMemory("pika.exe+3C7D", "bytes", "0x7C 0x0D"); //P2 trigger off (Default)
+                }
+            }
+            else
+            {
+                MessageBox.Show("Run the pika.exe");
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            Mem memory = new Mem();
+            int PID = memory.GetProcIdFromName("pika.exe"); //프로세스 아이디
+
+            if (PID > 0)
+            {
+                memory.OpenProcess(PID);
+
+                if (comboBox1.SelectedIndex == 0) //Player 1
+                {
+                    memory.WriteMemory("pika.exe+3C7D", "bytes", "0xEB 0x00");//P1 trigger on
+                }
+
+                if (comboBox1.SelectedIndex == 1) //Player 2
+                {
+                    memory.WriteMemory("pika.exe+3C7D", "bytes", "0xEB 0x0D");//P2 trigger on
+                }
+            }
+            else
+            {
+                MessageBox.Show("Run the pika.exe");
+            }
+        }
+    }
+}
